21df8482306fec987b86d958eb72e320309a4e7e
viranboi <168511383+Viranboi@users.noreply.github.com>
2025-07-07 10:12:49 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'savauser',
-  password: 'savapassword',
-  database: 'savamovies'
+  host: process.env.RAILWAY_PRIVATE_DOMAIN || '127.0.0.1',
+  user: process.env.MYSQLUSER || 'savauser',
+  password: process.env.MYSQL_ROOT_PASSWORD || 'savapassword',
+  database: process.env.MYSQL_DATABASE || 'savamovies',
+  port: 3306,
 });
 
 db.connect(err => {
